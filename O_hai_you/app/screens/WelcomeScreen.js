afd5131ff0ec6c91b5cd3053b4f73e8d92c57a00
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-09 12:26:34 -0400

    created welcome page

---
+++ O_hai_you/app/screens/WelcomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, ImageBackground, StyleSheet, Image } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background_welcome_page.jpeg")}
+    ></ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+});
+
+export default WelcomeScreen;

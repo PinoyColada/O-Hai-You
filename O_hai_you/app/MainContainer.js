2e7935165ba3542678a77f985f8bf24a23107a18
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-12 13:40:05 -0400

    created pronunciation screen with audio button 'a'

--- O_hai_you/app/MainContainer.js
+++ O_hai_you/app/MainContainer.js
@@ -6,10 +6,12 @@ import IonIcons from "react-native-vector-icons/Ionicons";
 import LessonScreen from "./navigation/components/LessonScreen";
 import ProfileScreen from "./navigation/components/ProfileScreen";
 import TranslatorScreen from "./navigation/components/TranslatorScreen";
+import PronunciationScreen from "./navigation/components/PronunciationScreen";
 
 const lessonName = "Lesson";
 const profileName = "Profile";
 const translatorName = "Translator";
+const pronunciationName = "Prnunciation";
 
 const Tab = createBottomTabNavigator();
 
@@ -29,6 +31,8 @@ export default function MainContainer() {
               iconName = focused ? "ios-person" : "ios-person-outline";
             } else if (rn === translatorName) {
               iconName = focused ? "language" : "language-outline";
+            } else if (rn === pronunciationName) {
+              iconName = focused ? "chatbubbles" : "chatbubbles-outline";
             }
             return <IonIcons name={iconName} size={size} color={color} />;
           },
@@ -43,6 +47,7 @@ export default function MainContainer() {
         <Tab.Screen name={lessonName} component={LessonScreen} />
         <Tab.Screen name={profileName} component={ProfileScreen} />
         <Tab.Screen name={translatorName} component={TranslatorScreen} />
+        <Tab.Screen name={pronunciationName} component={PronunciationScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ O_hai_you/app/navigation/components/PronunciationScreen.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Text, SafeAreaView, StyleSheet, Button } from "react-native";
+import { Audio } from "expo-av";
+
+function PronunciationScreen() {
+  const [sound, setSound] = useState();
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/Hiragana_a.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <SafeAreaView>
+      <Button title="Play Sound" onPress={playSound} />
+    </SafeAreaView>
+  );
+}
+
+export default PronunciationScreen;

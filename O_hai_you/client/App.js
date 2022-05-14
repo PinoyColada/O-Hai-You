26ff04f514f7f721649c29b02f21d4a923363559
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-13 23:59:14 -0400

    moved node modules/dependencies to right places

--- O_hai_you/client/App.js
+++ O_hai_you/client/App.js
@@ -37,7 +37,7 @@ export default function App() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator
-        initialRouteName="Log In"
+        initialRouteName="MainContainer"
         screenOptions={{
           headerShown: false,
         }}

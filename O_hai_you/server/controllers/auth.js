4108e1c83bd6b8b32c62857300147ff050bd8ba6
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-13 23:52:35 -0400

    created functionality for user to update password

--- O_hai_you/server/controllers/auth.js
+++ O_hai_you/server/controllers/auth.js
@@ -196,3 +196,25 @@ exports.uploadImage = async (req, res) => {
     console.log(err);
   }
 };
+
+// Update a password of a user
+exports.updatePassword = async (req, res) => {
+  try {
+    const { password } = req.body;
+    if (password && password.length < 6) {
+      return res.json({
+        error: "Password is required and should be min 6 characters long",
+      });
+    } else {
+      const hashedPassword = await hashPassword(password);
+      const user = await User.findByIdAndUpdate(req.user._id, {
+        password: hashedPassword,
+      });
+      user.password = undefined;
+      user.secret = undefined;
+      return res.json(user);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};

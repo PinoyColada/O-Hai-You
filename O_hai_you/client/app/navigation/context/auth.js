a1cee9e39279a2b08403b1bc49836edb815ad046
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-05-14 23:48:12 -0400

    created auth file on front end

---
+++ O_hai_you/client/app/navigation/context/auth.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API } from "../config";
+import { useNavigation } from "@react-navigation/native";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [state, setState] = useState({
+    user: null,
+    token: "",
+  });
+
+  const navigation = useNavigation();
+
+  const token = state && state.token ? state.token : "";
+  axios.defaults.baseURL = API;
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  axios.interceptors.response.use(
+    async function (response) {
+      return response;
+    },
+    async function (error) {
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        await AsyncStorage.removeItem("@auth");
+        setState({ user: null, token: "" });
+        navigation.navigate("Signin");
+      }
+    }
+  );
+
+  useEffect(() => {
+    const loadFromAsyncStorage = async () => {
+      let data = await AsyncStorage.getItem("@auth");
+      const as = JSON.parse(data);
+      setState({ ...state, user: as.user, token: as.token });
+    };
+    loadFromAsyncStorage();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={[state, setState]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
